1726e56e9173142fa0462f947a49bcad317ce36f
Wazingwa Mugala <wazingwamugala90@gmail.com>
2026-01-02 12:23:32 +0200

    Enhance authentication routes with improved error handling and logout functionality

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ import expess from 'express';
 import bycrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import pool from '../db.js';
+import { protect } from '../middleware/auth.js';
 
 
 const router = expess.Router();
@@ -42,4 +43,48 @@ router.post('/register', async (req, res) => {
     const token = generateToken(newUser.rows[0]);
 
     res.cookie('token', token, cookieOptions);
+
+    return res.status(201).json({ user: newUser.rows[0]});
 });
+
+// Login
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Please provide email and password' });
+    }
+    const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+    if (user.rows.length === 0) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const userData = user.rows[0];
+
+    const isMatch = await bycrypt.compare(password, user.rows[0].password);
+
+    if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const token = generateToken(userData.id);
+
+    res.cookie('token', token, cookieOptions);
+
+    res.json({ user: {id: userData.id, username: userData.username, email: userData.email} });
+});
+
+// Me
+
+router.get('/me', protect, async (req, res) => {
+    res.json(req.user);
+    // return s the info of the loogied in use
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+    res.cookie('token', '', { ...cookieOptions, maxAge: 1 });
+    res.json({ message: 'Logged out successfully' });
+});
+
+export default router;
